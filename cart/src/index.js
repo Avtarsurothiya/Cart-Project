27b6998b251d99742be29678895316abfb190697
Avtarsurothiya <avtarsurothiya@gmail.com>
2023-08-08 04:20:07 +0530

    writing first component CartItem

--- cart/src/index.js
+++ cart/src/index.js
@@ -5,16 +5,6 @@ import App from './App';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
-
-
-
-
-
-
-
-
-
-
  const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
