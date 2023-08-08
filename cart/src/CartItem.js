93d083c122f52216fff2f2aaf1fac076bc5b0150
Avtarsurothiya <avtarsurothiya@gmail.com>
2023-08-08 17:51:08 +0530

    Addintg increse and decrease qty funcationality

--- cart/src/CartItem.js
+++ cart/src/CartItem.js
@@ -1,31 +1,97 @@
 import React from "react";
 class CartItem extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      price: 999,
+      price: 70000,
       title: 'Mobile Phone',
       qty: 1,
       img: ''
     }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
+  //Below promise make setState work synchronous and works as an API call, its just for testing purposes.
+  // testing () {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('done');
+  //     }, 5000);
+  //   })
+
+  //   promise.then(() => {
+  //     // setState acts like a synchronus call
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     console.log('state', this.state);
+  //   });
+  // }
+
+  increaseQuantity = () => {
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // });
+
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
+  }
+  decreaseQuantity = () => {
+   const { qty } =this.state;
+   if (qty === 1){
+    return;
+   }
+
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
+    });
+  }
+
   render() {
     const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image} />
+          <img
+            style={styles.image}
+            src="https://rukminim2.flixcart.com/image/850/1000/ktketu80/mobile/q/a/u/iphone-13-pro-mlw03hn-a-apple-original-imag6vpbvmfygne9.jpeg?q=20" />
         </div>
 
         <div className="right-block">
-        <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>Rs {price} </div>
-          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: '#777' }}>Rs {price} </div>
+          <div style={{ color: '#777' }}>Qty: {qty} </div>
           <div className="cart-item-actions">
             {/* Buttons */}
-            <img alt="incease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/4210/4210903.png" />
-            <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1665/1665765.png" />
-            <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/6096/6096937.png" />
+            <img
+              alt="incease"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/4210/4210903.png"
+              onClick={this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/1665/1665765.png"
+              onClick={this.decreaseQuantity}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/6096/6096937.png"
+            />
           </div>
         </div>
       </div>
