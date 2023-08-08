a35af5e35b26bf188a208e64946f85f5fbf39d0b
Avtarsurothiya <avtarsurothiya@gmail.com>
2023-08-08 22:31:52 +0530

    Adding Navbar

---
+++ cart/src/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+class Navbar extends React.Component {
+
+ 
+  render() {
+    
+    return (
+     <div className="navbar">
+        <div>
+            <img src="https://cdn-icons-png.flaticon.com/128/6387/6387870.png" alt="cart-icon"/>
+            <span>3</span>
+        </div>
+     </div>
+    );
+  }
+}
+
+
+
+export default Navbar;
